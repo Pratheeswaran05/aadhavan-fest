966905dd8eebdbbc11b6bb2bb0f69f8020eb5811
Pratheeswaran05 <pratheeswaransubramani@gmail.com>
2025-03-30 02:08:33 +0530

    update signup components

--- aadhavanfest/src/app/features/admin/signup/signup.component.ts
+++ aadhavanfest/src/app/features/admin/signup/signup.component.ts
@@ -8,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class SignupComponent {
 
+  name: string = '';
+  email: string = '';
+  selectedRole: string = 'student';
+  roleId: string = '';
+  password: string = '';
+  confirmPassword: string = '';
+  showPassword: boolean = false;
+
+  // Dynamic labels and placeholders
+  roleLabel: string = 'Student ID';
+  rolePlaceholder: string = 'Enter your Student ID';
+
+  constructor() {}
+
+  // Form submit method
+  register() {
+    console.log('Registering:', {
+      name: this.name,
+      email: this.email,
+      role: this.selectedRole,
+      roleId: this.roleId,
+      password: this.password,
+    });
+  }
+
+  // Toggle password visibility
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
 }
+
