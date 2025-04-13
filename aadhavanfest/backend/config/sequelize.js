497eb4b4dd6cec0c1fe3bbc4eb12790fcd30c372
MANEESHWARAN SARAVANAN <maneeshwaran01@gmail.com>
2025-04-13 18:16:03 +0530

    render connection

--- aadhavanfest/backend/config/sequelize.js
+++ aadhavanfest/backend/config/sequelize.js
@@ -1,3 +1,23 @@
+// const { Sequelize } = require('sequelize');
+// require('dotenv').config();
+
+// const sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASS,
+//     {
+//         host: process.env.DB_HOST,
+//         dialect: 'postgres',
+//         port: process.env.DB_PORT,
+//         logging: false,
+//     }
+// );
+
+
+// module.exports = sequelize;
+
+
+// render connection
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
@@ -10,7 +30,17 @@ const sequelize = new Sequelize(
         dialect: 'postgres',
         port: process.env.DB_PORT,
         logging: false,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
     }
 );
 
 module.exports = sequelize;
+
+sequelize.authenticate()
+  .then(() => console.log('Database connected successfully 🚀'))
+  .catch(err => console.error('Database connection failed ❌', err));
